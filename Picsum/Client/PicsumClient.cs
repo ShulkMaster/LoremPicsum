da33aabe7123d0fbcf5b1b4df0651cd702aa8242
shulkmaster <shulkmaster@gmail.com>
2024-11-02 15:28:03 -0600

    Refactor namespaces and add PicsumClient.
    Renamed LoremUrl namespace to ShulkMaster.Picsum.Url and adjusted its internal structure. Added a new PicsumClient class with methods for fetching and saving data from Lorem Picsum URLs. Updated project settings including RootNamespace and C# language version.

---
+++ Picsum/Client/PicsumClient.cs
@@ -0,0 +1,49 @@
+﻿using ShulkMaster.Picsum.Url;
+
+namespace ShulkMaster.Picsum.Client;
+
+public static class PicsumClient
+{
+  private static readonly HttpClient Client = new();
+
+  public static Task<Stream> GetStream(LoremUrl url, CancellationToken token = default)
+  {
+    return Client.GetStreamAsync(url.Value, token);
+  }
+
+  public static async Task SaveToFileAsync(LoremUrl url, string filePath, CancellationToken token = default)
+  {
+    Directory.CreateDirectory(filePath);
+    var fStream = new FileStream(filePath, FileMode.Create);
+    await SaveToStreamAsync(url, fStream, token);
+    fStream.Close();
+    await fStream.DisposeAsync();
+  }
+  
+  public static void SaveToFile(LoremUrl url, string filePath)
+  {
+    Directory.CreateDirectory(filePath);
+    var fStream = new FileStream(filePath, FileMode.Create);
+    SaveToStream(url, fStream);
+    fStream.Close();
+    fStream.Dispose();
+  }
+
+  public static async Task SaveToStreamAsync(LoremUrl url, Stream outStream, CancellationToken token = default)
+  {
+    await using var stream = await Client.GetStreamAsync(url.Value, token);
+    await stream.CopyToAsync(outStream, token);
+    await outStream.FlushAsync(token);
+  }
+  
+  public static void SaveToStream(LoremUrl url, Stream outStream)
+  {
+    using var stream = Client
+      .GetStreamAsync(url.Value)
+      .ConfigureAwait(false)
+      .GetAwaiter()
+      .GetResult();
+    stream.CopyTo(outStream);
+    outStream.Flush();
+  }
+}
---
+++ Picsum/Url/LoremUrl.cs
@@ -0,0 +1,11 @@
+﻿namespace ShulkMaster.Picsum.Url;
+
+public readonly struct LoremUrl
+{
+  public readonly string Value;
+
+  internal LoremUrl(string value)
+  {
+    Value = value;
+  }
+}
